2d693fc9d7bc225bb90ee5a52917b44e6a3be52f
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-03-27 13:39:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
+const relatives=['John','David','Peter','Samantha']
 const App = () => {
+   
   return (
     <div id="main">
                <ol key="relativeList">
-                <li key="relativeListItem1">John</li>
-                 <li key="relativeListItem2">David</li>
-                  <li key="relativeListItem3">peter</li>
-                   <li key="relativeListItem4">Samantha</li>
-               </ol>
+                {relatives.map((values,index)=>(
+                  <li key={`realtiveListItem${index+1}`}>{values}</li>
+                ))}
+                </ol>
     </div>
   )
 }
